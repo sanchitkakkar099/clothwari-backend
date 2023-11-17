32514ad9fa4d5fd954e7f1db339e2e162dc79fc5
gaurav mendhare <gauravmendhare15@gmail.com>
2023-11-17 21:41:36 +0530

    changes

--- src/controllers/designupload.controller.js
+++ src/controllers/designupload.controller.js
@@ -79,10 +79,36 @@ exports.designuploadById = async (req, res) => {
                 }
             },
             {
+                $lookup: {
+                    from: 'fileuploads',
+                    localField: 'image',
+                    foreignField: '_id',
+                    as: 'image',
+                }
+            },
+            {
+                $unwind: { path: "$image", preserveNullAndEmptyArrays: true }
+            },
+            {
+                $lookup: {
+                    from: 'fileuploads',
+                    localField: 'thumbnail',
+                    foreignField: '_id',
+                    as: 'thumbnail',
+                }
+            },
+            {
+                $unwind: { path: "$thumbnail", preserveNullAndEmptyArrays: true }
+            },
+            {
                 $project: {
                     _id: "$_id",
                     label: "$name",
-                    value: "$code"
+                    value: "$code",
+                    thumbnail: '$thumbnail',
+                    image: '$image',
+                    designNo: "designNo"
+
                 }
             },
             {
